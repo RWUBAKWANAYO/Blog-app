ba7167adb066c5203969e880f3a773a383f07216
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-09-02 10:28:46 +0200

    fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = params[:post_id]
-    
+
     if @comment.save
       redirect_to user_post_comments_path(current_user, @comment.post_id)
     else
