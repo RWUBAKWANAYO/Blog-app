50c458482273011e916c125f0e7636f0d4a20e2c
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-09-08 10:19:32 +0200

    install and set up gem 'devise'

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do   
+  devise_for :users
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
   end
-  
   root to: 'users#index'
 end
